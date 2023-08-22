1e232d1e57210ca66c1ba4fb77b3933e8be8980a
ponta10 <mponta2002@gmail.com>
2023-08-23 06:54:27 +0900

    add style

--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,170 @@
-"use client"
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 20px;
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  width: 80%;
+  margin-right: 10px;
+`;
+
+const Button = styled.button`
+  padding: 10px 15px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const TodoList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+
+const TodoItem = styled.li`
+  padding: 10px;
+  border: 1px solid #ddd;
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const EditContainer = styled.div`
+  margin-top: 20px;
+`;
 
 interface TodoProps {
-    id: number;
-    title: string;
-    completed: boolean;
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
 const Home: React.FC = () => {
-    const [todos, setTodos] = useState<TodoProps[]>([]);
-    const [newTodo, setNewTodo] = useState<string>('');
-    const [selectedTodo, setSelectedTodo] = useState<TodoProps | null>(null);
-    const [editTitle, setEditTitle] = useState<string>('');
+  const [todos, setTodos] = useState<TodoProps[]>([]);
+  const [newTodo, setNewTodo] = useState<string>("");
+  const [selectedTodo, setSelectedTodo] = useState<TodoProps | null>(null);
+  const [editTitle, setEditTitle] = useState<string>("");
 
-    const fetchTodos = async () => {
-        try {
-            const response = await axios.get('/todo');
-            setTodos(response.data);
-        } catch (error) {
-            console.error('Error fetching todos:', error);
-        }
-    };
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("/todo");
+      setTodos(response.data);
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
 
-    const handleAddTodo = async () => {
-        if (newTodo) {
-            try {
-                const response = await axios.post('/todo', {
-                    title: newTodo
-                });
-                const addedTodo = response.data;
-                setTodos([...todos, addedTodo]);
-                setNewTodo('');
-            } catch (error) {
-                console.error('Error adding todo:', error);
-            }
-        }
-    };
+  const handleAddTodo = async () => {
+    if (newTodo) {
+      try {
+        const response = await axios.post("/todo", {
+          title: newTodo,
+        });
+        const addedTodo = response.data;
+        setTodos([...todos, addedTodo]);
+        setNewTodo("");
+      } catch (error) {
+        console.error("Error adding todo:", error);
+      }
+    }
+  };
 
-    const handleShow = async (id: number) => {
-        try {
-            const response = await axios.get(`/todo/${id}`);
-            setSelectedTodo(response.data);
-        } catch (err) {
-            console.error('Error fetching todo:', err);
-        }
-    };
+  const handleShow = async (id: number) => {
+    try {
+      const response = await axios.get(`/todo/${id}`);
+      setSelectedTodo(response.data);
+    } catch (err) {
+      console.error("Error fetching todo:", err);
+    }
+  };
 
-    const handleUpdate = async () => {
-        if (selectedTodo && editTitle) {
-            try {
-                const response = await axios.put(`/todo/${selectedTodo.id}`, {
-                    title: editTitle
-                });
-                const updatedTodo = response.data;
-                setTodos(todos.map(todo => todo.id === updatedTodo.id ? updatedTodo : todo));
-                setSelectedTodo(null);
-                setEditTitle('');
-            } catch (error) {
-                console.error('Error updating todo:', error);
-            }
-        }
-    };
+  const handleUpdate = async () => {
+    if (selectedTodo && editTitle) {
+      try {
+        const response = await axios.put(`/todo/${selectedTodo.id}`, {
+          title: editTitle,
+        });
+        const updatedTodo = response.data;
+        setTodos(
+          todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
+        );
+        setSelectedTodo(null);
+        setEditTitle("");
+      } catch (error) {
+        console.error("Error updating todo:", error);
+      }
+    }
+  };
 
-    const handleDelete = async (id: number) => {
-        try {
-            await axios.delete(`/todo/${id}`);
-            setTodos(todos.filter(todo => todo.id !== id));
-            setSelectedTodo(null);
-        } catch (error) {
-            console.error('Error deleting todo:', error);
-        }
-    };
+  const handleDelete = async (id: number) => {
+    try {
+      await axios.delete(`/todo/${id}`);
+      setTodos(todos.filter((todo) => todo.id !== id));
+      setSelectedTodo(null);
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
+  };
 
-    useEffect(() => {
-        fetchTodos();
-    }, []);
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-    return (
-        <div>
-            <h1>TODO アプリ</h1>
-            
-            <input 
-                value={newTodo} 
-                onChange={e => setNewTodo(e.target.value)} 
-                placeholder="新しいタスク"
-            />
-            
-            <button onClick={handleAddTodo}>追加</button>
+  return (
+    <Container>
+      <Title>TODO アプリ</Title>
 
-            <ul>
-                {todos.map((todo, idx) => (
-                    <li key={idx} onClick={() => handleShow(todo.id)}>
-                        {todo.title}
-                        <button onClick={() => handleDelete(todo.id)}>削除</button>
-                    </li>
-                ))}
-            </ul>
+      <div>
+        <Input
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="新しいタスク"
+        />
 
-            {selectedTodo && (
-                <div>
-                    <h2>タスクの編集</h2>
-                    <input 
-                        value={editTitle} 
-                        onChange={e => setEditTitle(e.target.value)} 
-                        placeholder="タスクのタイトルを編集"
-                    />
-                    <button onClick={handleUpdate}>更新</button>
-                </div>
-            )}
-        </div>
-    );
-}
+        <Button onClick={handleAddTodo}>追加</Button>
+      </div>
+
+      <TodoList>
+        {todos.map((todo, idx) => (
+          <TodoItem key={idx}>
+            <span onClick={() => handleShow(todo.id)}>{todo.title}</span>
+            <Button onClick={() => handleDelete(todo.id)}>削除</Button>
+          </TodoItem>
+        ))}
+      </TodoList>
+
+      {selectedTodo && (
+        <EditContainer>
+          <h2>タスクの編集</h2>
+          <Input
+            value={editTitle}
+            onChange={(e) => setEditTitle(e.target.value)}
+            placeholder="タスクのタイトルを編集"
+          />
+          <Button onClick={handleUpdate}>更新</Button>
+        </EditContainer>
+      )}
+    </Container>
+  );
+};
 
 export default Home;
